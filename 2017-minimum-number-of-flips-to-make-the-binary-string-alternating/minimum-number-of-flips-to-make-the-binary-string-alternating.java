b5e8f80454f85e5f35c17e49903875990e3453bb
Argha Dutta <123383407+dutta-argha@users.noreply.github.com>
2026-03-07 19:29:25 +0530

    Time: 14 ms (99.58%) | Memory: 46.8 MB (89.83%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -9,7 +9,7 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             op[(s.charAt(i) ^ i) & 1]--;
-            op[(s.charAt(i) ^ (n + i)) & 1]++;
+            op[(s.charAt(i) ^ (n + i-2)) & 1]++;
             res = Math.min(res, Math.min(op[0], op[1]));
         }
 
